3155e99443b175c2b010ac2c02178f0a31fd6ace
raythomass <rthomas5678@gmail.com>
2026-07-05 17:56:12 -0700

    styled season selector and details

--- client/src/components/ShowEpisode.jsx
+++ client/src/components/ShowEpisode.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 export default function ShowEpisode({episode}) {
   return (
-    <div>
-        {episode.name}
+    <div className='episode-cont'>
+      <img src={`https://image.tmdb.org/t/p/original${episode.still_path}`} alt="still picture of the episode"/>
+      <div className='episode-details'>
+        <h4>{episode.episode_number}. {episode.name}</h4>
+        <div>
+          <small>{episode.runtime} minutes</small>
+        </div>
+        <p>{episode.overview}</p>
+      </div>
     </div>
   );
 }
